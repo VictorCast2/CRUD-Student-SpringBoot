55224801a00c21ec0274e9281c354156dfe8c714
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-09-12 20:25:41 -0500

    PromedioNotas

--- TestMaster/src/main/java/com/School/TestMaster/Services/Teachers/TeachersServices.java
+++ TestMaster/src/main/java/com/School/TestMaster/Services/Teachers/TeachersServices.java
@@ -53,4 +53,17 @@ public class TeachersServices {
         teachersRepository.deleteById(id);
     }}
 
+    /**
+     * Guarda un profesor.
+    *
+    * @param Nota30 esta es la nota del 30% del estudiante.
+    *
+    * @param Nota70 esta es la nota del 70% del estudiante.
+    *
+    * @return El promedio de la nota final.
+    **/
+    public Double PromedioDelEstudiante(Double Nota30, Double Nota70){
+        return Double.parseDouble((Nota30*0.3)+(Nota70*0.7));
+    }
+
 }
