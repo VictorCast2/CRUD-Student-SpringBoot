6826e5783a5e54abbe6a5b8d4cd3aab25e0ca496
VictorCast2 <castillocastrovictorjose@hotmail.com>
2024-09-13 00:24:36 -0500

    Crud-SpringBoot

--- TestMaster/src/main/java/com/School/TestMaster/Repository/Students/StudentsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.School.TestMaster.Repository.Students;
-
-import com.School.TestMaster.Model.Students.StudentsModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface StudentsRepository extends JpaRepository<StudentsModel, Long> {
-}
--- TestMaster/src/main/java/com/School/TestMaster/Repository/Teachers/TeachersRepository.java
+++
@@ -1,9 +0,0 @@
-package com.School.TestMaster.Repository.Teachers;
-
-import com.School.TestMaster.Model.Teachers.TeachersModel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TeachersRepository extends JpaRepository<TeachersModel, Long> {
-}
